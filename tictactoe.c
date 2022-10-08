e83bf60b9a239f558b2a4772bdc8407e1bff4f8a
Parker Solome <pjsolome@cpp.edu>
2022-10-07 17:17:47 -0700

    Added Welcome screen and the ability to choose pvp or pvc

--- tictactoe.c
+++ tictactoe.c
@@ -1,6 +1,24 @@
+#include <stdio.h>
+
 // Welcome Screen
+void welcome(){
+printf("=========================== \n \n");
+printf("WELCOME TO TIC TAC TOE! \n \n");
+printf("1 -- person vs person \n \n");
+printf("2 -- person vs random computer \n \n");
+printf("Enter your choice (1 or 2): ");
+}
 
 // Choose pvp vs pvc
+void choose(){
+static int choice;
+
+fflush(stdout);
+scanf("%d", &choice);
+
+printf("=========================== \n \n");
+printf("You have entered choice %d \n \n", choice);
+}
 
 // Show status of board
 
@@ -9,3 +27,9 @@
 // pvc moves
 
 // End of game and replay prompt
+
+//Main
+void main(){
+    welcome();
+    choose();
+}
